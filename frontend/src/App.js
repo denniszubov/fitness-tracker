bafef12b95ecd1428a616ce470e554014b7a6539
Dennis Zubov <denniszubov365@gmail.com>
2024-04-08 13:10:12 -0400

    fe:Delete unused files and add simple routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import RunsList from './Runs';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <RunsList />
-    </div>
+    <Routes>
+      <Route path="run/" element={<RunsList />} />
+    </Routes>
   );
 }
 
